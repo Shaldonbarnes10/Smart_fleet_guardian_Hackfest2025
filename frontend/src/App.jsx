9ff279fa9ab156222fad9453d247744b79cdc917
SHaldonbarnes1 <nnm23cs172@nmamit.in>
2025-04-19 10:34:40 +0530

    final commit before moving to lab! All the best !!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing"
 import { AuthProvider } from "./auth-context";
 import AuthForm from "./components/auth/AuthForm";
+import Statistics from "./pages/StatisticsPage";
 import Dashboard from "./pages/Dashboard";
 // Import other components as needed
 
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Landing />} />
           <Route path="/Auth" element={<AuthForm />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/statistics" element={<Statistics />} />
           {/* Add other routes as needed */}
         </Routes>
       </Router>
